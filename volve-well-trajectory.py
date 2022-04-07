c8456a00930fb6afbd29e0525dedba66e375490d
RoboIOTers <46434898+RoboIOTers@users.noreply.github.com>
2022-04-08 01:25:55 +0530

    Update volve-well-trajectory.py

--- volve-well-trajectory.py
+++ volve-well-trajectory.py
@@ -34,5 +34,5 @@ for well in wells:
 
 df_all_wells['nmd']=df_all_wells['md']*-1
 ps.line_3d(df_all_wells,'dispNs','dispEw','nmd','well')
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.plotly_chart()
+fig = st.set_option('deprecation.showPyplotGlobalUse', False)
+st.plotly_chart(fig)
